aa35125e87f4626ed48a1fe5d978f6e2dc893765
chris.bartlett <chris.bartlett@acquia.com>
2019-06-14 20:02:26 -0700

    added an ignore to 'src/'

--- repl.h
+++ repl.h
@@ -1,3 +1,10 @@
+/*
+  * Copyright (c) 2019 Christopher Bartlett
+  * [This program is licensed under the "MIT License"]
+  * Please see the file LICENSE in the source
+  * distribution of this software for license terms.
+*/
+
 #ifndef REPL
 
 #define REPL
